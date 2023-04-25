975fff774b1694ccfcce9c71b97e95d5783795dd
Lucas Israel <lisraelalves92@gmail.com>
2023-04-25 20:59:28 -0300

    feat: :sparkles: challenge 6 done

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    finish = len(word)
+    to_return = True
+    for index, _ in enumerate(range(0, finish)):
+        if word[index] != word[finish - (1 + index)]:
+            to_return = False
+
+    return to_return
