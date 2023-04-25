ea08c4782b1e6095d4b1d324230b61f2e13031ad
Lucas Israel <lisraelalves92@gmail.com>
2023-04-25 16:29:02 -0300

    refactor: :recycle: removing manual test

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -8,9 +8,3 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word[low_index] != word[high_index]:
         return False
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-
-
-if __name__ == "__main__":
-    word = "aba"
-    a = is_palindrome_recursive(word, 0, len(word) - 1)
-    print(a)
