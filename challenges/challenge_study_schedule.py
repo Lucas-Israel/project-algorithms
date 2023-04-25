41753b2360c3571b6b9b8b96e1a57ce177e13af4
Lucas Israel <lisraelalves92@gmail.com>
2023-04-25 15:09:06 -0300

    feat: :sparkles: challenge one done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period, target_time="a"):
+    count = 0
+    try:
+        for element in permanence_period:
+            if int(target_time) in range(element[0], element[1] + 1):
+                count += 1
+    except (TypeError, ValueError, AssertionError):
+        return None
+    return count
