4950df4b4c14f099d8e179c01d4e20fa6c204ea3
Lucas Israel <lisraelalves92@gmail.com>
2023-04-25 15:53:21 -0300

    feat: :sparkles: challenge 2 done

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+
+key_error_msg = "tipo inválido para key"
+msg_error_msg = "tipo inválido para message"
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message(message="abcde", key=3) == "cba_ed"
+    assert encrypt_message(message="abcde", key=2) == "edc_ba"
+    assert encrypt_message(message="abcde", key=5) == "edcba"
+    assert encrypt_message("edcba", 5) != "edcba"
+
+    with pytest.raises(TypeError, match=key_error_msg):
+        encrypt_message("abcc", "abcde")
+
+    with pytest.raises(TypeError, match=msg_error_msg):
+        encrypt_message(2, 2)
