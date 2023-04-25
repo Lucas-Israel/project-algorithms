a6e657a22aab8faab4ef599f19303352bd94cc3c
Lucas Israel <lisraelalves92@gmail.com>
2023-04-25 16:28:10 -0300

    feat: :sparkles: challenge 3 done

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    if len(word) == 1:
+        return True
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+if __name__ == "__main__":
+    word = "aba"
+    a = is_palindrome_recursive(word, 0, len(word) - 1)
+    print(a)
