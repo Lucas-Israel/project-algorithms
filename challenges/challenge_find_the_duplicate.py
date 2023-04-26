390950ba947609515d6a55161986b60fa1894ef6
Lucas Israel <lisraelalves92@gmail.com>
2023-04-26 12:14:46 -0300

    refactor: :recycle: possible refactor for challenge 5 v2

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -6,62 +6,33 @@ class Found_Duplicate(Exception):
     pass
 
 
-def merge_sort(numbers, start=0, end=None):
-    if not isinstance(numbers[0], int) or numbers[0] < 0:
-        raise CustomError
-    if end is None:
-        end = len(numbers)
-    if (end - start) > 1:
-        mid = (start + end) // 2
-        merge_sort(numbers, start, mid)
-        merge_sort(numbers, mid, end)
-        merge(numbers, start, mid, end)
-
-
-def merge(numbers, start, mid, end):
-    left = numbers[start:mid]
-    right = numbers[mid:end]
-
-    left_index, right_index = 0, 0
-
-    for general_index in range(start, end):
-        if left_index >= len(left):
-            numbers[general_index] = right[right_index]
-            right_index = right_index + 1
-        elif right_index >= len(right):
-            numbers[general_index] = left[left_index]
-            left_index = left_index + 1
-        elif left[left_index] < right[right_index]:
-            numbers[general_index] = left[left_index]
-            left_index = left_index + 1
-        else:
-            numbers[general_index] = right[right_index]
-            right_index = right_index + 1
-
-
 def len_check(nums):
     if len(nums) <= 1:
         raise CustomError
 
 
-def duplicate_checker(nums):
-    for i, ele in enumerate(nums):
-        if i == 0:
-            pass
-        if nums[i - 1] == ele:
-            return ele
-    return False
+def dict_creator(nums):
+    new_dict = {}
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            raise CustomError
+        new_dict[num] = new_dict.get(num, 0) + 1
+        if new_dict[num] > 1:
+            raise Found_Duplicate(num)
+    return new_dict
 
 
 def find_duplicate(nums):
     try:
         len_check(nums)
-        merge_sort(nums)
+        dict_creator(nums)
 
     except CustomError:
         return False
+    except Found_Duplicate as found:
+        return found.args[0]
 
-    return duplicate_checker(nums)
+    return False
 
 
 if __name__ == "__main__":
